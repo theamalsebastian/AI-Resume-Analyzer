c9831bd86abfb538074b98a93c566cc68c852f84
Amal Sebastian <amalsebastian1420@gmail.com>
2026-05-24 12:22:33 +0530

    Commit this change

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+    
+
 import streamlit as st
 import PyPDF2
 import spacy
